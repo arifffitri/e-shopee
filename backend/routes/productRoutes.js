471ada15fcc7a15ae26cf5dba009c8837cc726ec
arifffitri <arifffitri.mn@gmail.com>
2023-04-17 21:26:32 +0800

    39 - Create Product (Admin)

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -12,6 +12,30 @@ productRouter.get("/", async (req, res) => {
   res.send(products);
 });
 
+// route for adding new product (Admin)
+
+productRouter.post(
+  "/",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const newProduct = new Product({
+      name: "sample name " + Date.now(),
+      slug: "sample-name-" + Date.now(),
+      image: "/images/p1.jpg",
+      price: 0,
+      category: "sample category",
+      brand: "sample brand",
+      countInStock: 0,
+      rating: 0,
+      numReviews: 0,
+      description: "sample description",
+    });
+    const product = await newProduct.save();
+    res.send({ message: "Product Created", product });
+  })
+);
+
 // route for search/filter
 // PAGE_SIZE = want to display how many product per page
 const PAGE_SIZE = 3;
