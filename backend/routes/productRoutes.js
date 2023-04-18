f0653f4fd8e0db70b19531556fa176cbedfb6b52
arifffitri <arifffitri.mn@gmail.com>
2023-04-18 14:40:14 +0800

    41 - Implement Update Product (Admin)

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -13,7 +13,6 @@ productRouter.get("/", async (req, res) => {
 });
 
 // route for adding new product (Admin)
-
 productRouter.post(
   "/",
   isAuth,
@@ -36,10 +35,34 @@ productRouter.post(
   })
 );
 
+// route for updating existing product (Admin)
+productRouter.put(
+  ":/id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+    if (product) {
+      product.name = req.body.name;
+      product.slug = req.body.slug;
+      product.price = req.body.price;
+      product.image = req.body.image;
+      product.category = req.body.category;
+      product.brand = req.body.brand;
+      product.countInStock = req.body.countInStock;
+      product.description = req.body.description;
+      await product.save();
+      res.send({ message: "Product Updated" });
+    } else {
+      res.status(404).send({ message: "Product Not Found" });
+    }
+  })
+);
+
 // route for search/filter
 // PAGE_SIZE = want to display how many product per page
 const PAGE_SIZE = 3;
-
 productRouter.get(
   "/admin",
   isAuth,
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -18,21 +18,31 @@ const reducer = (state, action) => {
       return { ...state, loading: false };
     case "FETCH_FAIL":
       return { ...state, loading: false, error: action.payload };
+
+    case "UPDATE_REQUEST":
+      return { ...state, loadingUpdate: true };
+    case "UPDATE_SUCCESS":
+      return { ...state, loadingUpdate: false };
+    case "UPDATE_FAIL":
+      return { ...state, loadingUpdate: false };
+
     default:
       return state;
   }
 };
 
 export default function ProductEditScreen() {
+  const navigate = useNavigate();
   const params = useParams(); // /product/:id
   const { id: productId } = params;
 
   const { state } = useContext(Store);
   const { userInfo } = state;
 
-  const [{ loading, error }, dispatch] = useReducer(reducer, {
+  const [{ loading, error, loadingUpdate }, dispatch] = useReducer(reducer, {
     loading: true,
     error: "",
+    // loadingUpdate: true,
   });
 
   const [name, setName] = useState("");
@@ -68,6 +78,36 @@ export default function ProductEditScreen() {
     fetchData();
   }, [productId]);
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch({ type: "UPDATE_REQUEST" });
+      await axios.put(
+        `/api/products/${productId}`,
+        {
+          _id: productId,
+          name,
+          slug,
+          price,
+          image,
+          category,
+          brand,
+          countInStock,
+          description,
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({ type: "UPDATE_SUCCESS" });
+      toast.success("Product updated successfully");
+      navigate("/admin/products");
+    } catch (err) {
+      toast.error(getError(err));
+      dispatch({ type: "UPDATE_FAIL" });
+    }
+  };
+
   return (
     <Container className="small-container">
       <Helmet>
@@ -80,7 +120,7 @@ export default function ProductEditScreen() {
       ) : error ? (
         <MessageBox variant="danger">{error}</MessageBox>
       ) : (
-        <Form>
+        <Form onSubmit={submitHandler}>
           <Form.Group className="mb-3" controlId="name">
             <Form.Label>Name</Form.Label>
             <Form.Control value={name} onChange={(e) => setName(e.target.value)} required />
@@ -114,7 +154,10 @@ export default function ProductEditScreen() {
             <Form.Control value={description} onChange={(e) => setDescription(e.target.value)} required />
           </Form.Group>
           <div className="mb-3">
-            <Button type="submit">Update</Button>
+            <Button disabled={loadingUpdate} type="submit">
+              Update
+            </Button>
+            {loadingUpdate && <LoadingBox></LoadingBox>}
           </div>
         </Form>
       )}
