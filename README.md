3bb29aff0715a37ef80531bd02822f0f79b7f39f
arifffitri <arifffitri.mn@gmail.com>
2023-04-19 15:42:46 +0800

    46 - Delete Order (Admin)

--- README.md
+++ README.md
@@ -187,3 +187,7 @@
     1. add deliver button
     2. handle click action
     3. implement backend api for deliver
+46. Delete Order (Admin)
+    1. add delete button
+    2. handle click action
+    3. implement backend api for delete
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -147,4 +147,21 @@ orderRouter.put(
     }
   })
 );
+
+// route for deleting order (Admin)
+orderRouter.delete(
+  "/:id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      await order.deleteOne();
+      res.send({ message: "Order Deleted" });
+    } else {
+      res.status(404).send({ message: "Order Not Found" });
+    }
+  })
+);
+
 export default orderRouter;
